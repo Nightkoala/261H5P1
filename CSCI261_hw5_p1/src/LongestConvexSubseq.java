71f0a96c52a1fa0b3952553be7dc27476f59e650
Derek Brown <djb3718@rit.edu>
2014-11-04 13:51:33 -0500

    Added java file

---
+++ CSCI261_hw5_p1/src/LongestConvexSubseq.java
@@ -0,0 +1,23 @@
+/**
+ * LongestConvexSubseq.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu)
+ * 
+ * Purpose:	Given an input consisting of integers, find the maximum length of a
+ * 			convex subsequence.
+ */
+
+import java.util.Scanner;
+
+public class LongestConvexSubseq {
+
+	// Attributes
+	
+	// Constructor
+	
+	// Methods
+
+	public static void main(String[] args) {
+
+	}// end main
+}//end LongestConvexSubseq
